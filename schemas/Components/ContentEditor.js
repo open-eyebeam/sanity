e0f0f542549179404bb5369254f7bba1ce7cb60f
pwrstudio <pwrpntr@gmail.com>
2022-03-15 13:44:15 +0100

    Require alt-text. Close #4

--- schemas/Components/ContentEditor.js
+++ schemas/Components/ContentEditor.js
@@ -111,7 +111,8 @@ export default {
                             type: 'string',
                             options: {
                                 isHighlighted: true
-                            }
+                            },
+                            validation: Rule => Rule.required(),
                         },
                         {
                             title: 'Caption',
