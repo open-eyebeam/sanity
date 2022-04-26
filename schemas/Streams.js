21090f3950b1a6607fea9958fcaa7cc4badc005a
pwrstudio <pwrpntr@gmail.com>
2022-04-26 16:02:05 +0200

    Streams

--- schemas/Streams.js
+++ schemas/Streams.js
@@ -25,6 +25,7 @@ export default {
                         title: "Active",
                         name: "active",
                         type: "boolean",
+                        initialValue: false
                     },
                     {
                         title: "Room or Zone",
@@ -43,15 +44,42 @@ export default {
                                 { title: "Audio", value: "audio" }
                             ],
                         },
+                        initialValue: 'video',
                     },
                     {
                         title: "URL",
                         description: "URL for a stream hosted by peertube, youtube or vimeo.",
                         name: "videoUrl",
                         type: "url",
-                        validation: Rule => Rule.required(),
+                        hidden: ({ parent }) => parent?.streamType !== "video",
                     },
-                ]
+                    {
+                        title: "Audio file",
+                        name: "audioFile",
+                        type: "file",
+                        hidden: ({ parent }) => parent?.streamType !== "audio",
+                    },
+                    {
+                        title: "Image",
+                        name: "image",
+                        type: "image",
+                        hidden: ({ parent }) => parent?.streamType !== "audio",
+                    },
+                ],
+                preview: {
+                    select: {
+                        title: 'title',
+                        active: 'active'
+                    },
+                    prepare(selection) {
+                        const { title, active } = selection
+                        return {
+                            title: title,
+                            subtitle: active ? 'Active' : 'Inactive'
+                        }
+                    },
+                }
+
             }]
         },
     ]
